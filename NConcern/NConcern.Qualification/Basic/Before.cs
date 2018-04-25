10385e4eaed6a0b4048464748d0b27f5aaabbd8d
Jens <jho1965us@gmail.com>
2018-04-25 21:30:58 +0200

    Many parameters problem
    Added Test for problem in CNeptune: problem when instance method or
    constructor has more than three
    parameters

--- NConcern/NConcern.Qualification/Basic/Before.cs
+++ NConcern/NConcern.Qualification/Basic/Before.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Reflection;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using System.Threading;
@@ -221,6 +222,53 @@ namespace NConcern.Qualification.Basic
                 Aspect.Release<Before.Interceptor.Parameterized>(_method);
                 Interception.Initialize();
                 _instance.Method(2, 3);
+                Assert.AreEqual(Interception.Done, false);
+            }
+        }
+
+        [TestMethod]
+        public void BasicBeforeManyArgumentsInstanceMethod()
+        {
+            lock (Interception.Handle)
+            {
+                var _method = Metadata<ManyArguments>.Method(_ManyArguments => _ManyArguments.Instance(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+                var _values = new[] {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+                var _manyArguments = new ManyArguments();
+                Interception.Initialize();
+                var _return = _manyArguments.Instance(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+                Assert.IsTrue(_return.SequenceEqual(_values));
+                Assert.AreEqual(Interception.Done, false);
+                Aspect.Weave<Before.Interceptor>(_method);
+                Interception.Initialize();
+                _return = _manyArguments.Instance(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+                Assert.IsTrue(_return.SequenceEqual(_values));
+                Assert.AreEqual(Interception.Done, true);
+                Aspect.Release<Before.Interceptor>(_method);
+                Interception.Initialize();
+                _manyArguments.Instance(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+                Assert.AreEqual(Interception.Done, false);
+            }
+        }
+
+        [TestMethod]
+        public void BasicBeforeManyArgumentsStaticMethod()
+        {
+            lock (Interception.Handle)
+            {
+                var _method = Metadata<ManyArguments>.Method(_ManyArguments => ManyArguments.Static(0, 1, 2, 3, 4, 5, 6, 7, 8, 9));
+                var _values = new[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+                Interception.Initialize();
+                var _return = ManyArguments.Static(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+                Assert.IsTrue(_return.SequenceEqual(_values));
+                Assert.AreEqual(Interception.Done, false);
+                Aspect.Weave<Before.Interceptor>(_method);
+                Interception.Initialize();
+                _return = ManyArguments.Static(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
+                Assert.IsTrue(_return.SequenceEqual(_values));
+                Assert.AreEqual(Interception.Done, true);
+                Aspect.Release<Before.Interceptor>(_method);
+                Interception.Initialize();
+                ManyArguments.Static(0, 1, 2, 3, 4, 5, 6, 7, 8, 9);
                 Assert.AreEqual(Interception.Done, false);
             }
         }
---
+++ NConcern/NConcern.Qualification/ManyArguments.cs
@@ -0,0 +1,14 @@
+﻿namespace NConcern.Qualification
+{
+    public class ManyArguments
+    {
+        static public int[] Static(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9)
+        {
+            return new[] {a0, a1, a2, a3, a4, a5, a6, a7, a8, a9};
+        }
+        public int[] Instance(int a0, int a1, int a2, int a3, int a4, int a5, int a6, int a7, int a8, int a9)
+        {
+            return new[] {a0, a1, a2, a3, a4, a5, a6, a7, a8, a9};
+        }
+    }
+}
